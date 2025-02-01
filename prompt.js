076c1f8a0995c5ad21683d944cb3d3a3574cb900
Nikol <nikolhurd@gmail.com>
2025-02-01 15:14:56 +0100

    camelCase change

--- prompt.js
+++ prompt.js
@@ -3,20 +3,21 @@ import inquirer from "inquirer";
 const questions = [
   {
     type: "input",
-    name: "pokemon-name",
+    name: "pokemonName",
     message: "Pokemon name",
   },
   {
     type: "checkbox",
-    name: "pokemon-info",
+    name: "pokemonInfo",
     message: "Pokemon info to download",
     choices: [{ name: "Stats" }, { name: "Sprites" }, { name: "Artwork" }],
   },
 ];
 
 const promptUser = () => {
-  // ask user for name of pokemon
-  // https://pokeapi.co/api/v2/pokemon/{name}/
+  //  https://pokeapi.co/api/v2/pokemon/`${pokemonName}`
 };
 
-inquirer.prompt(questions);
+inquirer.prompt(questions).then((answers) => {
+  console.log(answers);
+});
